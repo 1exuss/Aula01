a1451dd664c9f527cfc7c15e087480b4f97e45e4
Natan Alexandre Zacharko Werkauser <nw0115424@setrem.com.br>
2021-09-04 09:18:52 -0300

    first commit

---
+++ imc/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace imc
+{
+	public class cidade
+	{
+		public int cep { get; set; }
+		public string descricao { get; set; }
+		public char uf { get; set; }
+
+	}
+	class Program
+	{
+	
+
+
+
+
+
+
+
+
+
+
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("<==Calcula IMC==>");
+			calcula();
+
+		}
+		static void calcula()
+		{
+			Console.WriteLine("Informe sua Altura em CM");
+			double altura = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Informe seu Peso em KG");
+			double Peso = Convert.ToDouble(Console.ReadLine());
+			var altura2 = (altura * altura);
+			double diferenca = (Peso / altura2);
+			var resultado = diferenca.ToString().Substring(0, 2);
+			Console.WriteLine($@"Seu IMC é {resultado}");
+		}
+	}
+}
